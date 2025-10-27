01526aba64ba7a700a92505b1ab257796ba8296c
Samf33 <144719150+Samf33@users.noreply.github.com>
2025-10-27 09:02:24 -0400

    done

--- src/average_temperature.py
+++ src/average_temperature.py
@@ -3,8 +3,9 @@
 import pandas as pd
 
 def average_temperature():
-    pass
-
+    df = pd.read_csv("src/kumpula-weather-2017.csv", index_col=0)
+    print(df.iloc[181:212,6])
+    return df.iloc[181:212,6].mean()
 def main():
     result = average_temperature()
     print(f"Average temperature in July: {result:.1f}")
